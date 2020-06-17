cd1f2fd6d7095d2c681efa2bdc030f2620ff4e6b
Clark Chen <yyunqingchenn@gmail.com>
2020-06-17 16:22:24 -0400

    added profile component, not yet working

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -22,6 +22,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/">Home</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          {/* <Link to="/profile">Profile</Link> */}
         </div>
       )}
     </nav>
