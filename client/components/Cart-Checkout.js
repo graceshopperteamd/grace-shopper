69bd5670c0ebfaf8e3f2b5bc53fb7fde15f73079
Tiffany Sanchez <tiffanytha.sj@gmail.com>
2020-06-22 14:14:36 -0400

    Added edit quantity and remove buttons to cart page items

--- client/components/Cart-Checkout.js
+++ client/components/Cart-Checkout.js
@@ -4,13 +4,60 @@ import {connect} from 'react-redux'
 import {fetchCart} from '../store/shoppingCart'
 import {checkoutForm} from './Checkout-Form'
 
+// cart page component
 class Cart extends React.Component {
   constructor(props) {
     super(props)
+    this.createCartDiv = this.createCartDiv.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
   }
-
   componentDidMount() {
     this.props.getCart()
+  }
+
+  // console.log('',);
+
+  handleSubmit(event) {
+    let qty = document.getElementById('cartQty').value
+    console.log('qty', qty)
+
+    if (!qty) alert('Please indicate the qty of items you wish to add')
+    else {
+      alert('submit', {event})
+    }
+  }
+
+  handleRemove(event) {
+    alert('remove', {event})
+  }
+
+  // factory func maps through all products in shopping cart and creates a component for each
+  createCartDiv(cartArray) {
+    return cartArray.map(item => (
+      <div key={item.id} className="cartItems">
+        <img src={item.imageUrl} />
+        <div>
+          <div>Product: {item.name}</div>
+          <div>Price: ${item.price}</div>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="cartQty">Quantity</label>
+            <input
+              type="number"
+              id="cartQty"
+              min="1"
+              max="20"
+              placeholder="Qty"
+            />
+            <br />
+            <button type="submit">Add To Cart</button>
+          </form>
+          <button onClick={this.handleRemove}>Remove</button>
+        </div>
+      </div>
+    ))
   }
 
   render() {
@@ -18,10 +65,10 @@ class Cart extends React.Component {
       return (
         <div>
           <h2> My Shopping Cart </h2>
-          <div>{createCartDiv(this.props.shoppingcart)}</div>
+          <div>{this.createCartDiv(this.props.shoppingcart)}</div>
 
           <div className="cartPageBtns">
-            <Link to="/">
+            <Link to="/products">
               <button type="button">Back to Browse</button>
             </Link>
             <Link to="/checkout">
@@ -34,13 +81,27 @@ class Cart extends React.Component {
   }
 }
 
+// checkout page component
 class Checkout extends React.Component {
   constructor(props) {
     super(props)
+    this.createCheckoutDiv = this.createCheckoutDiv.bind(this)
   }
-
   componentDidMount() {
     this.props.getCart()
+  }
+
+  // factory func maps through all products in shopping cart and creates a component for each
+  createCheckoutDiv(cartArray) {
+    return cartArray.map(item => (
+      <div key={item.id} className="cartItems">
+        <img src={item.imageUrl} />
+        <div>
+          <div>Product: {item.name}</div>
+          <div>Price: ${item.price}</div>
+        </div>
+      </div>
+    ))
   }
 
   render() {
@@ -48,24 +109,13 @@ class Checkout extends React.Component {
       return (
         <div>
           <h2> Checkout </h2>
-          <div>{createCartDiv(this.props.shoppingcart)}</div>
+          <div>{this.createCheckoutDiv(this.props.shoppingcart)}</div>
+          {/* Checkout form component which is imported from /components/Checkout-Form */}
           <div>{checkoutForm}</div>
         </div>
       )
     else return <div>No items to Checkout</div>
   }
-}
-
-const createCartDiv = cartArray => {
-  return cartArray.map(item => (
-    <div key={item.id} className="cartItems">
-      <img src={item.imageUrl} />
-      <div>
-        <div>Product: {item.name}</div>
-        <div>Price: ${item.price}</div>
-      </div>
-    </div>
-  ))
 }
 
 const mapStateToProps = state => ({
--- client/components/homepage.js
+++ client/components/homepage.js
@@ -16,7 +16,6 @@ export class HomePage extends React.Component {
         <h3>Hello World!</h3>
 
         <p>
-          {' '}
           You are not logged in, please log in to see your personalized home
           page
         </p>
