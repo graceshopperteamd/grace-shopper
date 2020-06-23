aa059987aa627bdc516f1c1b589acb3293303c0e
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-23 19:16:44 -0400

    chore: implemented action/reducer for deleting product from db

--- client/store/product.js
+++ client/store/product.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
 const GET_ONE_PRODUCT = 'GET_ONE_PRODUCT'
+const REMOVE_ONE_PRODUCT = 'REMOVE_ONE_PRODUCT'
 
 const gotProducts = products => {
   return {
@@ -17,10 +18,22 @@ const gotOneProduct = currProduct => {
   }
 }
 
+const removeProduct = productId => ({
+  type: REMOVE_ONE_PRODUCT,
+  productId
+})
+
 export const fetchProducts = () => {
   return async dispatch => {
     const {data} = await axios('/api/products')
     dispatch(gotProducts(data))
+  }
+}
+
+export const deleteProduct = productId => {
+  return async dispatch => {
+    await axios.delete(`/api/products/${productId}`)
+    dispatch(removeProduct(productId))
   }
 }
 
@@ -30,6 +43,11 @@ export default function productsReducer(state = products, action) {
   switch (action.type) {
     case GET_ALL_PRODUCTS: {
       return action.products
+    }
+    case REMOVE_ONE_PRODUCT: {
+      console.log('action', action)
+      console.log('state', state)
+      return state.filter(product => product.id !== action.productId)
     }
     default:
       return state
