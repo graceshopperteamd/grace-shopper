de3ac0bfa4cdf02477da9495356d7d02de229652
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-23 16:57:22 -0400

    fix: fixed bug allowing non-users/basic users to access api

--- server/authenticate.js
+++ server/authenticate.js
@@ -1,11 +1,11 @@
 function authRole(role) {
   return (req, res, next) => {
-    if (req.user.role !== role) {
+    if (req.user && req.user.role === role) {
+      next()
+    } else {
       res.status(401) // restricted access, you are not allowed.
       return res.send('Not allowed here, bud. Go on, get!')
     }
-
-    next()
   }
 }
 
