a461c0668de6d0b5fd3b53bb280a39b8706e05f2
Tatiana Aviles <tavilesa12@gmail.com>
2020-06-24 10:19:21 -0400

    last commit

--- client/components/Single-Product.js
+++ client/components/Single-Product.js
@@ -62,19 +62,6 @@ class OneProduct extends React.Component {
   render() {
     let prodQty = this.props.currProduct.amount
 
-    // <Card bg='ligth' border='secondary' text="dark" style={{ width: '18rem' }} className="mb-2">
-    //   <Card.Body>
-
-    //     <div className="allProducts">
-    //       <Link to={`/products/${props.product.id}`}>
-    //         <Card.Title>{props.product.name}</Card.Title>
-    //         <Card.Img variant="top" src={props.product.imageUrl} />
-    //       </Link>
-    //       <Card.Text>price: ${props.product.price}</Card.Text>
-    //     </div>
-    //   </Card.Body>
-    // </Card>
-
     return (
       // className is for easy acces in css styling later
       <div className="singleProduct">
