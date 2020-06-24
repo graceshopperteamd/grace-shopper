e16f97e55219ce3b32e54d5ea04fb401e44cd112
Tiffany Sanchez <tiffanytha.sj@gmail.com>
2020-06-24 13:08:45 -0400

    Fixed accidental commit to master, will now merge local branch to master

--- client/components/Quantity-Change-Form.js
+++ client/components/Quantity-Change-Form.js
@@ -1,15 +1,65 @@
 import React from 'react'
 
-export const QtyForm = props => {
-  return (
-    <div>
-      <form onSubmit={props.handleSubmit} name={name} className="qtyForm">
-        <label htmlFor={props.name}>Quantity</label>
-        <input type="number" id={props.name} min="1" max="20" placeholder={1} />
-        <br />
-        <button type="submit">Edit Qty</button>
+export class QtyForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('New Product quantity was submitted' + this.state.value)
+    event.preventDefault()
+    doSomethingWithEvent(event)
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        name={props.name + 'QtyForm'}
+        className="qtyForm"
+      >
+        <label>
+          Quantity:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
-      <button onClick={props.handleRemove}>Remove</button>
-    </div>
-  )
+    )
+  }
 }
+
+// import React from 'react'
+
+// export const QtyForm = props => {
+
+//   return (
+//     <div>
+//       <form  name={props.name + 'QtyForm'} onSubmit={props.handleSubmit}className="qtyForm">
+//         <label htmlFor={props.name}>Quantity</label>
+//         <input
+//           type="number"
+//           id={props.name}
+//           name={props.name + 'Qty'}
+//           min="1"
+//           max="20"
+//           placeholder={1}
+//         />
+//         <br />
+//         <button type="submit">Edit Qty</button>
+//         <button onClick={props.handleSubmit} id={props.name} qty={0}>Remove</button>
+//       </form>
+//     </div>
+//   )
+// }
